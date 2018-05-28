4bc8aae93353bd43abcc5e30ea94f78201e19404
Grano <willia.cesar@granostudio.com.br>
2018-05-27 23:29:58 -0300

    finalização versão 1.0

--- js/index.js
+++ js/index.js
@@ -1,71 +1,104 @@
 
+// Animação referente ao menu e as setas ==============================
+
 $(document).ready(function(){
-	$( "#menu-item1" ).click(function() {
-	  $( this ).addClass( "change-menu" );
-	  $( "#menu-item2").removeClass( "change-menu" );
-	  $( "#menu-item3").removeClass( "change-menu" );
-	  $( "#menu-item4").removeClass( "change-menu" );
-	  $('html, body').animate({
-            scrollTop: $('#div1').offset().top -0
-        }, 500);
+	$( ".botao-menu" ).click(function() {
+	  $( this ).toggleClass( "change-botao-menu" );
+	  $( ".menu" ).toggleClass( "change-menu" );
 	});
-    $( "#menu-item2" ).click(function() {
-	  $( this ).addClass( "change-menu" );
-	  $( "#menu-item1").removeClass( "change-menu" );
-	  $( "#menu-item3").removeClass( "change-menu" );
-	  $( "#menu-item4").removeClass( "change-menu" );
+
+	$( ".list-menu1" ).click(function() {
 	  $('html, body').animate({
-            scrollTop: $('#div2').offset().top -0
-        }, 500);
-	});
-	$( "#menu-item3" ).click(function() {
-	  $( this ).addClass( "change-menu" );
-	  $( "#menu-item2").removeClass( "change-menu" );
-	  $( "#menu-item1").removeClass( "change-menu" );
-	  $( "#menu-item4").removeClass( "change-menu" );
-	  $('html, body').animate({
-            scrollTop: $('#div3').offset().top -0
-        }, 500);
-	});
-	$( "#menu-item4" ).click(function() {
-	  $( this ).addClass( "change-menu" );
-	  $( "#menu-item2").removeClass( "change-menu" );
-	  $( "#menu-item3").removeClass( "change-menu" );
-	  $( "#menu-item1").removeClass( "change-menu" );
-	  $('html, body').animate({
-            scrollTop: $('#div4').offset().top -0
-        }, 500);
-	});
-	$( "#seta1" ).click(function() {
-	  $('html, body').animate({
-            scrollTop: $('#div2').offset().top -0
+            scrollTop: $('.div1').offset().top -0
         }, 1000);
 	});
+
+	$( ".list-menu2" ).click(function() {
+	  $('html, body').animate({
+            scrollTop: $('.div2').offset().top -50
+        }, 1000);
+	});
+
+	$( ".list-menu3" ).click(function() {
+	  $('html, body').animate({
+            scrollTop: $('.div3').offset().top -0
+        }, 1000);
+	});
+
+	$( ".list-menu4" ).click(function() {
+	  $('html, body').animate({
+            scrollTop: $('.div4').offset().top -0
+        }, 1000);
+	});
+
+	$( "#seta1" ).click(function() {
+	  $('html, body').animate({
+            scrollTop: $('.div2').offset().top -0
+        }, 1000);
+	});
+
 	$( "#seta2" ).click(function() {
 	  $('html, body').animate({
-            scrollTop: $('#div3').offset().top -0
+            scrollTop: $('.div3').offset().top -0
+        }, 1000);
+	});
+
+	$( "#seta3" ).click(function() {
+	  $('html, body').animate({
+            scrollTop: $('.div4').offset().top -0
         }, 1000);
 	});
 
 });
 
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
 
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+    if ($(".div1").isInViewport()) {
+    	$( ".change-menu ul li" ).removeClass( "click-li" );
+	  	$( ".list-menu1" ).addClass( "click-li" );
+    }
+    if ($(".div2").isInViewport()) {
+    	$( ".change-menu ul li" ).removeClass( "click-li" );
+	  	$( ".list-menu2" ).addClass( "click-li" );
+    }
+    if ($(".div3").isInViewport()) {
+    	$( ".change-menu ul li" ).removeClass( "click-li" );
+	  	$( ".list-menu3" ).addClass( "click-li" );
+    }
+    if ($(".div4").isInViewport()) {
+    	$( ".change-menu ul li" ).removeClass( "click-li" );
+	  	$( ".list-menu4" ).addClass( "click-li" );
+    }
+});
+
+
+// Animação referente a div 2 ========================================
 
 $(window).scroll(function () {
-        $('.typewriter-text,.div-pika').each(function (i) {
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            /* se o objeto estiver completamente "scrollado" pra dentro da janela, fazer o fade_in*/
-            if (bottom_of_window > bottom_of_object) {
-                $( ".typewriter-text" ).addClass("line-1");
-                $( ".div-espec" ).css({"opacity":"1"});
-                $( "#seta2" ).css({"opacity":"1"});
-            }
-        });
+    $('.typewriter-text').each(function (i) {
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        /* se o objeto estiver completamente "scrollado" pra dentro da janela, fazer o fade_in*/
+        if (bottom_of_window > bottom_of_object) {
+            $( ".typewriter-text" ).addClass("line-1");
+            $( ".div-espec" ).css({"opacity":"1"});
+            $( "#seta2" ).css({"opacity":"1"});
+            $( "#seta3" ).css({"opacity":"1"});
+        }
     });
+});
 
 
-
+// Plugin AOS Animation ==============================================
 
 $(document).ready(function(){
   AOS.init({ disable: 'mobile' });
